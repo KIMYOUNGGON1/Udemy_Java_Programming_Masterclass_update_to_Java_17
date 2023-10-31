a81d3d3785d3d401de1cc547060441ab4ec7468d
KIMYOUNGGON <dudrhs950@naver.com>
2023-10-31 17:39:13 +0900

    20231031 lecture and challenge

---
+++ src/section_7/Dog.java
@@ -0,0 +1,30 @@
+package section_7;
+
+public class Dog extends Animal{
+
+    private String earShape;
+    private String tailShape;
+
+    public Dog(String type, double weight) {
+        this(type, weight, "Perkey", "Curled");
+    }
+
+    public Dog(String type, double weight, String earShape, String tailShape) {
+        super(type, weight < 15 ? "small" : (weight < 35 ? "medium" : "large"),
+                weight);
+        this.earShape = earShape;
+        this.tailShape = tailShape;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "earShape='" + earShape + '\'' +
+                ", tailShape='" + tailShape + '\'' +
+                "} " + super.toString();
+    }
+
+    public Dog() {
+        super("Mutt", "Big", 50);
+    }
+}
