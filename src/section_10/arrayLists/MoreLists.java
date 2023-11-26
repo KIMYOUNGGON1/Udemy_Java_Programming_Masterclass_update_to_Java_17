81af93bd38ac02d78205bfd8a22471c275f4e09d
KIMYOUNGGON <dudrhs950@naver.com>
2023-11-26 13:21:45 +0900

    20231126 lecture

--- src/section_10/arrayLists/MoreLists.java
+++ src/section_10/arrayLists/MoreLists.java
@@ -20,5 +20,21 @@ public class MoreLists {
 
         groceries.addAll(nextList);
         System.out.println(groceries);
+
+        System.out.println("Third item = " + groceries.get(2));
+
+        if (groceries.contains("mustard")) {
+            System.out.println("List contains mustard");
+        }
+
+        groceries.add("yogurt");
+        System.out.println("first = " + groceries.indexOf("yogurt"));
+        System.out.println("last = " + groceries.lastIndexOf("yogurt"));
+
+        System.out.println(groceries);
+        groceries.remove(1);
+        System.out.println(groceries);
+        groceries.remove("yogurt");
+        System.out.println(groceries);
     }
 }
