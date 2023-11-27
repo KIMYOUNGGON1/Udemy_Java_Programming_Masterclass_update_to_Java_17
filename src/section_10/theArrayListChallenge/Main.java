e0cd6ae8eff00f11279916c9e883204b2475a52b
KIMYOUNGGON <dudrhs950@naver.com>
2023-11-27 16:26:49 +0900

    20231127 lecture and challenge

--- src/section_10/theArrayListChallenge/Main.java
+++ src/section_10/theArrayListChallenge/Main.java
@@ -4,51 +4,73 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         ArrayList<String> groceryList = new ArrayList<>();
         boolean flag = true;
 
         while (flag) {
-            System.out.println("Available actions:\n" +
-                    "0 - to shutdown\n" +
-                    "1 - to add item(s) to list (comma delimited list)\n" +
-                    "2 - to remove any items (comma delimited list)\n" +
-                    "Enter a number for which action you want to do:");
+            printMenu();
+            int enteredInt = getIntInput();
 
-            int enteredInt = makeIntScannerObject();
-
-            if (enteredInt == 0) {
-                System.out.println("Grocery List: " + groceryList);
-                flag = false;
-            } else if (enteredInt == 1) {
-                System.out.println("Enter item(s) to add (comma delimited):");
-                String enteredString = makeStringScannerObject();
-                String[] itemsToAdd = enteredString.split(",");
-                for (String item : itemsToAdd) {
-                    if (!groceryList.contains(item.trim())) {
-                        groceryList.add(item.trim());
-                    }
-                }
-                System.out.println("Grocery List: " + groceryList);
-            } else if (enteredInt == 2) {
-                System.out.println("Enter item(s) to remove (comma delimited):");
-                String enteredString = makeStringScannerObject();
-                String[] itemsToRemove = enteredString.split(",");
-                for (String item : itemsToRemove) {
-                    groceryList.remove(item.trim());
-                }
-                System.out.println("Grocery List: " + groceryList);
+            switch (enteredInt) {
+                case 0:
+                    System.out.println(groceryList);
+                    flag = false;
+                    break;
+                case 1:
+                    addItems(groceryList);
+                    break;
+                case 2:
+                    removeItems(groceryList);
+                    break;
+                default:
+                    System.out.println("Invalid input. Please enter 0, 1, or 2.");
             }
         }
+
+        // Close the scanner after use
+        scanner.close();
     }
 
-    private static int makeIntScannerObject() {
-        Scanner scanner = new Scanner(System.in);
+    private static void printMenu() {
+        System.out.println("Available actions:");
+        System.out.println("0 - to shutdown");
+        System.out.println("1 - to add item(s) to list (comma delimited list)");
+        System.out.println("2 - to remove any items (comma delimited list)");
+        System.out.print("Enter a number for which action you want to do: ");
+    }
+
+    private static int getIntInput() {
         return scanner.nextInt();
     }
 
-    private static String makeStringScannerObject() {
-        Scanner scanner = new Scanner(System.in);
-        return scanner.next();
+    private static void addItems(ArrayList<String> list) {
+        System.out.print("Enter item(s) to add (comma delimited): ");
+        scanner.nextLine(); // Consume the newline character left by nextInt()
+        String enteredString = scanner.nextLine();
+        String[] items = enteredString.split(",");
+
+        for (String item : items) {
+            if (!list.contains(item.trim())) {
+                list.add(item.trim());
+            }
+        }
+
+        System.out.println("Updated list: " + list);
+    }
+
+    private static void removeItems(ArrayList<String> list) {
+        System.out.print("Enter item(s) to remove (comma delimited): ");
+        scanner.nextLine(); // Consume the newline character left by nextInt()
+        String enteredString = scanner.nextLine();
+        String[] items = enteredString.split(",");
+
+        for (String item : items) {
+            list.remove(item.trim());
+        }
+
+        System.out.println("Updated list: " + list);
     }
 }
