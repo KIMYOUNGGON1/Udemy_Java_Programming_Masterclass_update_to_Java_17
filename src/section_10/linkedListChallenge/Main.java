9bdf76929a3a01540e3ad98b6ff92e89188fcb01
KIMYOUNGGON <dudrhs950@naver.com>
2023-12-02 17:23:54 +0900

    20231202 lecture and challenge

--- src/section_10/linkedListChallenge/Main.java
+++ src/section_10/linkedListChallenge/Main.java
@@ -1,27 +1,55 @@
 package section_10.linkedListChallenge;
 
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         var townList = new LinkedList<Town>();
 
-        townList.add(new Town("Adelaide", "1374"));
-        townList.add(new Town("Alice Springs", "2771"));
-        townList.add(new Town("Brisbane", "917"));
-        townList.add(new Town("Darwin", "3972"));
-        townList.add(new Town("Melbourne", "877"));
-        townList.add(new Town("Perth", "3923"));
+        townList.add(new Town("Adelaide", 1374));
+        townList.add(new Town("Alice Springs", 2771));
+        townList.add(new Town("Brisbane", 917));
+        townList.add(new Town("Darwin", 3972));
+        townList.add(new Town("Melbourne", 877));
+        townList.add(new Town("Perth", 3923));
 
+        callScannerObject(townList);
     }
 
-    private static void callScannerObject() {
+    private static void callScannerObject(LinkedList<Town> linkedList) {
+        printScannerInputMessage();
         Scanner scanner = new Scanner(System.in);
-        String nextOperation = scanner.next();
+        String nextOperation = scanner.next().toLowerCase();
+
+        ListIterator<Town> iterator = linkedList.listIterator();
+
+        if (nextOperation.equals("f") || nextOperation.equals("forward")) {
+            while (iterator.hasNext()) {
+                System.out.println(iterator.next());
+            }
+        } else if (nextOperation.equals("b") || nextOperation.equals("backward")) {
+            while (iterator.hasPrevious()) {
+                System.out.println(iterator.previous());
+            }
+        } else if (nextOperation.equals("l") || nextOperation.equals("list places")) {
+            linkedList.forEach(System.out::println);
+        } else if (nextOperation.equals("q") || nextOperation.equals("quit")) {
+            scanner.close();
+        }
     }
 
     private static void printScannerInputMessage() {
-
+        System.out.println("""
+                Available actions (select word or letter) :
+                (F)orward
+                (B)ackward
+                (L)ist places
+                (M)enu
+                (Q)uit
+                """);
     }
 }
--- src/section_10/linkedListChallenge/Town.java
+++ src/section_10/linkedListChallenge/Town.java
@@ -2,11 +2,15 @@ package section_10.linkedListChallenge;
 
 public class Town {
     private String placeName;
-    private String distance;
+    private int distance;
 
-    public Town(String placeName, String distance) {
+    public Town(String placeName, int distance) {
         this.placeName = placeName;
         this.distance = distance;
     }
 
+    @Override
+    public String toString() {
+        return "Town : " + placeName + " / Distance from Sydney (in Km) : " + distance;
+    }
 }
