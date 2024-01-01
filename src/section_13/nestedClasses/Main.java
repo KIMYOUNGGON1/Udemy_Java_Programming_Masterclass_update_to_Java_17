edac8192d6d6c8f0e3103378d599d52a4c97b96f
KIMYOUNGGON <dudrhs950@naver.com>
2024-01-01 19:56:49 +0900

    20240101 lecture

--- src/section_13/nestedClasses/Main.java
+++ src/section_13/nestedClasses/Main.java
@@ -2,6 +2,7 @@ package section_13.nestedClasses;
 
 import section_13.nestedClasses.domain.Employee;
 import section_13.nestedClasses.domain.EmployeeComparator;
+import section_13.nestedClasses.domain.StoreEmployee;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,7 +27,21 @@ public class Main {
             System.out.println(e);
         }
 
+        System.out.println("Store Members");
 
+        List<StoreEmployee> storeEmployees = new ArrayList<>(List.of(
+                new StoreEmployee(10015, "Meg", 2019, "Target"),
+                new StoreEmployee(10515, "Joe", 2021, "Walmart"),
+                new StoreEmployee(10105, "Tom", 2020, "Macys"),
+                new StoreEmployee(10215, "Marty", 2018, "Walmart"),
+                new StoreEmployee(10322, "Bud", 2016, "Target")));
+
+        var comparator = new StoreEmployee().new StoreComparator<>();
+        storeEmployees.sort(comparator);
+
+        for (StoreEmployee e : storeEmployees) {
+            System.out.println(e);
+        }
 
     }
 }
