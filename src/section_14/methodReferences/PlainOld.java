59f9218fb192b61c2762fbd3a57d13fad0dd0f3c
KIMYOUNGGON <dudrhs950@naver.com>
2024-01-14 19:19:22 +0900

    20240114 lecture - What's a Method Reference?

---
+++ src/section_14/methodReferences/PlainOld.java
@@ -0,0 +1,13 @@
+package section_14.methodReferences;
+
+public class PlainOld {
+
+    private static int last_id = 1;
+
+    private int id;
+
+    public PlainOld() {
+        id = PlainOld.last_id++;
+        System.out.println("Creating a PlainOld Object: id = " + id);
+    }
+}
